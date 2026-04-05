2b5f78dbbdc7935762653e4fc4ff10c7d9bb93c4
Frontman <faresashraf20044@gmail.com>
2026-04-05 09:27:47 +0200

    testing something

--- frontend/src/data/english.ts
+++ frontend/src/data/english.ts
@@ -211,7 +211,7 @@ export const en = {
         },
     },
     hero: {
-        title: "Reuniting families, restoring hope",
+        title: "Reuniting families, restoring light",
         subtitle: "AI-powered reunification technology backed by the Ministry of Social Solidarity. We're here to help you bring them back.",
         placeholder: "Search for a missing person (Name, Image)...",
         searchButton: "Search Unify"
